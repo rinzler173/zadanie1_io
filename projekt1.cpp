ad0198b4fc91409e6f9f8872bd8349d843fab04a
rinzler173 <janbiesiada@sxterm.mat.umk.pl>
2024-03-10 14:06:30 +0100

    Dodano wywolanie czyPierwsza() do main

--- projekt1.cpp
+++ projekt1.cpp
@@ -17,16 +17,41 @@ bool czyPierwsza(int liczba) {
 
 int main() {
     int a, b;
-    cout << "Podaj liczbe calkowita ";
+    cout << "Podaj liczbe calkowita: ";
     cin >> a >> b;
+
     int wyjscie;
     do {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Sprawdz czy liczba jest pierwsza" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+        switch(wyjscie) {
+            case 1: {
+                int liczba;
+                cout << "Podaj liczbe do sprawdzenia: ";
+                cin >> liczba;
+                if(czyPierwsza(liczba)) {
+                    cout << liczba << " jest liczba pierwsza." << endl;
+                } else {
+                    cout << liczba << " nie jest liczba pierwsza." << endl;
+                }
+                break;
+            }
+            case 0: {
+                cout << "Koniec programu." << endl;
+                break;
+            }
+            default: {
+                cout << "Nieprawidlowy numer czynnosci. Sprobuj ponownie." << endl;
+                break;
+            }
+        }
     } while(wyjscie != 0);
+
     return 0;
 }
