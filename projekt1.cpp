a6b86fe8f6a876080925b00a7d82993f85d48f30
rinzler173 <janbiesiada@sxterm.mat.umk.pl>
2024-03-10 14:04:26 +0100

    Dodano funkcje czyPierwsza()

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+bool czyPierwsza(int liczba) {
+    if(liczba <= 1) {
+        return false;
+    }
+    for(int i = 2; i <= sqrt(liczba); ++i) {
+        if(liczba % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     int a, b;
